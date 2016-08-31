c1e3808ba29159f1e8ca41c0347dafbdcfc2b8bf
Jack Kinga <jackmwangi@gmail.com>
2016-08-31 23:58:35 +0300

    Can retrieve deeply nested fields

--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,11 @@ package utils
 
 import (
 	"encoding/json"
+	"strings"
+)
+
+var (
+	FIELD_SEP string = "."
 )
 
 func Parse(data []byte) (v interface{}, err error) {
@@ -9,6 +14,24 @@ func Parse(data []byte) (v interface{}, err error) {
 	return
 }
 
+func get(field string, v interface{}) interface{} {
+	switch t := v.(type) {
+	case map[string]interface{}:
+		return t[field]
+	}
+
+	return nil
+}
+
 func Get(field string, v interface{}) interface{} {
-	return v.(map[string]interface{})[field]
+	fields := strings.Split(field, FIELD_SEP)
+	result := v
+
+	for _, fieldStr := range fields {
+		result = get(fieldStr, result)
+		if result == nil {
+			return nil
+		}
+	}
+	return result
 }
