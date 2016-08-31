11a9e58fcaa21d79e5258de0f99b317cb8450c6c
Jack Kinga <jackmwangi@gmail.com>
2016-08-31 23:23:33 +0300

    can retrieve a first level field

--- utils/utils.go
+++ utils/utils.go
@@ -8,3 +8,7 @@ func Parse(data []byte) (v interface{}, err error) {
 	err = json.Unmarshal(data, &v)
 	return
 }
+
+func Get(field string, v interface{}) interface{} {
+	return v.(map[string]interface{})[field]
+}
