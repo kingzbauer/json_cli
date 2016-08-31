5d944f3a72fc04b7c86bcb12419c36d9b98e6b2f
Jack Kinga <jackmwangi@gmail.com>
2016-08-31 23:18:38 +0300

    unit test: can retrieve a first level field

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -29,5 +30,25 @@ func TestParse(t *testing.T) {
 	_, err = Parse([]byte(validJson))
 	if err == nil {
 		t.Errorf("Expected an error while parsing the content.")
+	}
+}
+
+func TestGet(t *testing.T) {
+	validJson := `{
+        "Id": "d836a5e40aa8974d7076e791ba3c14726bf2dd2cd079652477d6827973969130",
+        "Created": "2016-08-31T16:49:33.119587574Z",
+        "Path": "/bin/bash",
+        "Args": [],
+        "State": {
+            "Status": "running",
+            "Running": true,
+            "Paused": false
+        }
+     }`
+	v, _ := Parse([]byte(validJson))
+	expectedV := v.(map[string]interface{})["State"]
+	returnedV := Get("State", v)
+	if !reflect.DeepEqual(expectedV, returnedV) {
+		t.Errorf("Expected: %v, Got: %v", expectedV, returnedV)
 	}
 }
