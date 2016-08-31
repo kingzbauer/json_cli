2367b049f87338544313ea4924271eeefb44ed2d
Jack Kinga <jackmwangi@gmail.com>
2016-08-31 20:29:35 +0300

    test function for asserting proper feedback

---
+++ utils/utils_test.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	validJson := `{
+        "Id": "d836a5e40aa8974d7076e791ba3c14726bf2dd2cd079652477d6827973969130",
+        "Created": "2016-08-31T16:49:33.119587574Z",
+        "Path": "/bin/bash",
+        "Args": [],
+        "State": {
+            "Status": "running",
+            "Running": true,
+            "Paused": false,
+        }
+     }`
+
+	_, err := Parse([]byte(validJson))
+	if err != nil {
+		t.Errorf("Expected to parse content without error. Got %v instead.", err)
+	}
+
+	validJson = `{
+        "Id": "d836a5e40aa8974d7076e791ba3c14726bf2dd2cd079652477d6827973969130",
+        "Created": "2016-08-31T16:49:33.119587574Z",
+        "Path": "/bin/bash",`
+	_, err = Parse([]byte(validJson))
+	if err == nil {
+		t.Errorf("Expected an error while parsing the content.")
+	}
+}
