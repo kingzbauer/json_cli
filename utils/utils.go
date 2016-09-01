d8db5c17cf865a64c5b304d1311b1f70cba839e4
Jack Kinga <jackmwangi@gmail.com>
2016-09-01 11:03:15 +0300

    can index an array. Out of bounds are gracefully handled

--- utils/utils.go
+++ utils/utils.go
@@ -2,11 +2,14 @@ package utils
 
 import (
 	"encoding/json"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
 var (
 	FIELD_SEP string = "."
+	INDEX_REG        = regexp.MustCompile(`^\[\d+\]$`)
 )
 
 func Parse(data []byte) (v interface{}, err error) {
@@ -14,10 +17,29 @@ func Parse(data []byte) (v interface{}, err error) {
 	return
 }
 
+func isIndex(v string) bool {
+	return INDEX_REG.Match([]byte(v))
+}
+
+func retrieveValueFromIndex(v string) int {
+	// remote the opening bracket
+	v = strings.Replace(v, "[", "", 1)
+	v = strings.Replace(v, "]", "", 1)
+	intV, _ := strconv.Atoi(v)
+	return intV
+}
+
 func get(field string, v interface{}) interface{} {
 	switch t := v.(type) {
 	case map[string]interface{}:
 		return t[field]
+	case []interface{}:
+		if isIndex(field) {
+			index := retrieveValueFromIndex(field)
+			if index < len(t) {
+				return t[index]
+			}
+		}
 	}
 
 	return nil
