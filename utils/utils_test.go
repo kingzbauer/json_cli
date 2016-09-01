949b70d27bc0854e3dc404c828e0aa0dea952b77
Jack Kinga <jackmwangi@gmail.com>
2016-09-01 10:51:44 +0300

    unit test: can index an array. Out of bounds are gracefully handled

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	validJson := `{
+	validJSON := `{
         "Id": "d836a5e40aa8974d7076e791ba3c14726bf2dd2cd079652477d6827973969130",
         "Created": "2016-08-31T16:49:33.119587574Z",
         "Path": "/bin/bash",
@@ -18,23 +18,23 @@ func TestParse(t *testing.T) {
         }
      }`
 
-	_, err := Parse([]byte(validJson))
+	_, err := Parse([]byte(validJSON))
 	if err != nil {
 		t.Errorf("Expected to parse content without error. Got \"%v\" instead.", err)
 	}
 
-	validJson = `{
+	validJSON = `{
         "Id": "d836a5e40aa8974d7076e791ba3c14726bf2dd2cd079652477d6827973969130",
         "Created": "2016-08-31T16:49:33.119587574Z",
         "Path": "/bin/bash",`
-	_, err = Parse([]byte(validJson))
+	_, err = Parse([]byte(validJSON))
 	if err == nil {
 		t.Errorf("Expected an error while parsing the content.")
 	}
 }
 
 func TestGet(t *testing.T) {
-	validJson := `{
+	validJSON := `{
         "Id": "d836a5e40aa8974d7076e791ba3c14726bf2dd2cd079652477d6827973969130",
         "Created": "2016-08-31T16:49:33.119587574Z",
         "Path": "/bin/bash",
@@ -45,7 +45,7 @@ func TestGet(t *testing.T) {
             "Paused": false
         }
      }`
-	v, _ := Parse([]byte(validJson))
+	v, _ := Parse([]byte(validJSON))
 	expectedV := v.(map[string]interface{})["State"]
 	returnedV := Get("State", v)
 	if !reflect.DeepEqual(expectedV, returnedV) {
@@ -58,7 +58,7 @@ func TestGet(t *testing.T) {
 }
 
 func TestGetNestedFields(t *testing.T) {
-	validJson := `{
+	validJSON := `{
         "Id": "d836a5e40aa8974d7076e791ba3c14726bf2dd2cd079652477d6827973969130",
         "Created": "2016-08-31T16:49:33.119587574Z",
         "Path": "/bin/bash",
@@ -73,7 +73,7 @@ func TestGetNestedFields(t *testing.T) {
         }
      }`
 
-	v, _ := Parse([]byte(validJson))
+	v, _ := Parse([]byte(validJSON))
 	key := "State.Log.Level"
 	expectedV := v.(map[string]interface{})["State"].(map[string]interface{})["Log"].(map[string]interface{})["Level"]
 	returnedV := Get(key, v)
@@ -83,5 +83,53 @@ func TestGetNestedFields(t *testing.T) {
 
 	if Get("State.Log.Nonexisting", v) != nil {
 		t.Error("Expected a nil result")
+	}
+}
+
+func TestCanIndexAnArray(t *testing.T) {
+	validJSON := `{
+        "Id": "d836a5e40aa8974d7076e791ba3c14726bf2dd2cd079652477d6827973969130",
+        "Created": "2016-08-31T16:49:33.119587574Z",
+        "Path": "/bin/bash",
+        "Args": [
+            {"id": 56},
+            {"id": 23}
+        ],
+        "State": {
+            "Status": "running",
+            "Running": true,
+            "Paused": false
+        }
+     }`
+	v, _ := Parse([]byte(validJSON))
+	key := "Args.[1].id"
+	expectedV := v.(map[string]interface{})["Args"].([]interface{})[1].(map[string]interface{})["id"]
+	returnedV := Get(key, v)
+	if !reflect.DeepEqual(expectedV, returnedV) {
+		t.Errorf("Expected %v: Got %v", expectedV, returnedV)
+	}
+
+	// Test for index out of bounds
+	key = "Args.[2].id"
+	returnedV = Get(key, v)
+	if returnedV != nil {
+		t.Errorf("Expected a nil value for index out of bounds: Got \"%v\" instead", returnedV)
+	}
+
+	// Test for a non-array entry
+	key = "[1]"
+	returnedV = Get(key, v)
+	if returnedV != nil {
+		t.Errorf("Expected a nil value for index out of bounds: Got \"%v\" instead", returnedV)
+	}
+
+	// Test for a field with the syntax of indexing
+	validJSON = `{"[3]": 78}`
+	key = "[3]"
+	v, _ = Parse([]byte(validJSON))
+	expectedV = v.(map[string]interface{})[key]
+	returnedV = Get(key, v)
+	if !reflect.DeepEqual(expectedV, returnedV) {
+		t.Errorf("Expected %v: Got %v", expectedV, returnedV)
 	}
 }
