8f87664ccb9eb2595af0cd7d0dd87392f8208f6d
Jack Kinga <jackmwangi@gmail.com>
2016-09-01 13:34:31 +0300

    process values from terminal

---
+++ jsoncli/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+var (
+	key  = flag.String("k", "", "The key value for the field you want to access. Separate nested keys using `.`")
+	file = flag.String("f", "", "Json file to search")
+)
+
+func main() {
+	flag.Parse()
+	if len(*key) == 0 {
+		fmt.Printf("Field `%s` is required\n", "-k")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	var (
+		content []byte
+		err     error
+	)
+	if len(*file) > 0 {
+		content, err = ioutil.ReadFile(*file)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	} else {
+		// try reading from Stdin
+		stat, _ := os.Stdin.Stat()
+		if (stat.Mode() & os.ModeCharDevice) == 0 {
+			content, err = ioutil.ReadAll(os.Stdin)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+	}
+
+	// check if any content is present
+	if len(content) == 0 {
+		fmt.Println("No file content could be read. Either pass a file name via the `-f` flag or through stdin")
+	}
+}
