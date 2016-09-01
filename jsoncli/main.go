bdb0cb013af8747009f49fa193b847eaac695a35
Jack Kinga <jackmwangi@gmail.com>
2016-09-01 13:41:01 +0300

    parse json

--- jsoncli/main.go
+++ jsoncli/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/kingzbauer/json_cli/utils"
 )
 
 var (
@@ -45,5 +47,13 @@ func main() {
 	// check if any content is present
 	if len(content) == 0 {
 		fmt.Println("No file content could be read. Either pass a file name via the `-f` flag or through stdin")
+		os.Exit(0)
 	}
+
+	v, err := utils.Parse(content)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(utils.Get(*key, v))
 }
